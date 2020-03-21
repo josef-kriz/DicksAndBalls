fa0af475d8a27abd8e2d6ae5b7dfb8346931591d
Josef Kriz <email@josefkriz.com>
2020-03-21 23:18:07 +0100

    change dependencies

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,11 @@ import express from 'express'
 import socketIo from 'socket.io'
 import { gameListener } from './listeners/gameListener'
 
-const port = process.env.SERVER_PORT || 3001
+const port = process.env.PORT || 3001
 
 const app = express()
 
 app.use(express.static('./client/build'))
-app.get('/*', (req, res) => {
-    res.sendFile('./client/build/index.html');
-})
 
 const server = app.listen(port, () => {
     console.log(`Server is up and listening on port ${port}.`)
