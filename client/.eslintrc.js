fb0908a5c8d8aff0103ca70dda7cfd259fe5fc1a
Josef Kriz <email@josefkriz.com>
2020-03-22 10:26:25 +0100

    fix eslint monorepo problems

---
+++ client/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-console": "error"
+    }
+};
