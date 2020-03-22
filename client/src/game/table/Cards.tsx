b1d6fd640fc728c1f58ef9a19df1d44eb4bf9d72
Josef Kriz <email@josefkriz.com>
2020-03-22 13:08:00 +0100

    hide card when you don't have any

--- client/src/game/table/Cards.tsx
+++ client/src/game/table/Cards.tsx
@@ -16,9 +16,11 @@ interface Props {
     sendPlayerAction: (action: PlayerAction) => void
 }
 
-export const Cards: FC<Props> = (props: Props): ReactElement => {
+export const Cards: FC<Props> = (props: Props): ReactElement | null => {
     const [open, setOpen] = React.useState(false)
     const [pickedCard, setPickedCard] = React.useState<Card | undefined>()
+
+    if (props.cards.length === 0) return null
 
     const askForColor = (): void => {
         setOpen(true)
