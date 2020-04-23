0e84b1c714a95289920ca06af380559ce3511639
Josef Kriz <email@josefkriz.com>
2020-04-23 14:30:11 +0200

    css improvements

--- client-ionic/src/app/game/components/table/deck-card/deck-card.component.ts
+++ client-ionic/src/app/game/components/table/deck-card/deck-card.component.ts
@@ -23,15 +23,29 @@ export class DeckCardComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustStyle(`url("assets/cards/${getCardsAssetNumber(card)}.png")`)
   }
 
+  /**
+   * This function employs the Box–Muller transform to give a normal distribution between 0 and 1 inclusive.
+   */
+  private random_normal(): number {
+    let u = 0, v = 0;
+    while (u === 0) { u = Math.random(); } // Converting [0,1) to (0,1)
+    while (v === 0) { v = Math.random(); }
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) { return this.random_normal(); } // resample between 0 and 1
+    return num;
+  }
+
   private getRandomShift(): string | null {
     if (this.noTransform) { return null }
 
     const getRandomArbitrary = (min: number, max: number) => {
-      return Math.floor(Math.random() * Math.floor(max - min)) + min
+      const random = this.random_normal() * Math.floor(max - min) + min
+      return Math.floor(random * 1.8) // flattens the curve
     }
 
-    return `translateX(${getRandomArbitrary(-10, 11)}px) \
-            translateY(${getRandomArbitrary(-5, 5)}px) \
-            rotate(${getRandomArbitrary(-60, 61)}deg)`
+    return `translateX(${getRandomArbitrary(-16, 40)}px) \
+            translateY(${getRandomArbitrary(-24, 24)}px) \
+            rotate(${getRandomArbitrary(-180, 180)}deg)`
   }
 }
