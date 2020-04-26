e1fdafe825330ec6885e0e1bef5adc6c88518e34
Josef Kriz <email@josefkriz.com>
2020-04-26 19:03:43 +0200

    fix missing question mark

--- client-ionic/src/app/game/helpers/leaveGameGuard.ts
+++ client-ionic/src/app/game/helpers/leaveGameGuard.ts
@@ -14,6 +14,6 @@ export class LeaveGameGuard implements CanDeactivate<ComponentCanDeactivate> {
       // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
       // when navigating away from your angular app, the browser will show a generic warning message
       // see http://stackoverflow.com/a/42207299/7307355
-      confirm('WARNING: Leaving the game will stop the current game and interrupt it for all other players. Do you want to continue.');
+      confirm('WARNING: Leaving the game will stop the current game and interrupt it for all other players. Do you want to continue?');
   }
 }
