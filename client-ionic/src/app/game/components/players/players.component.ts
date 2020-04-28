e85622e1f0652dbb3cbb69e70470b08dc6ea879d
Josef Kriz <email@josefkriz.com>
2020-04-28 20:24:55 +0200

    fix import

--- client-ionic/src/app/game/components/players/players.component.ts
+++ client-ionic/src/app/game/components/players/players.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { Opponent } from '../../models/opponent'
+import { Opponent } from '../../../models/opponent'
 import { SettingsService } from '../../../settings/settings.service'
 
 @Component({
