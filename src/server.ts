64b448859d3e626d76f5f08fc68338c89fab4558
Josef Kriz <email@josefkriz.com>
2020-04-28 20:30:00 +0200

    prepare for deployment

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 
 const app = express()
 
-app.use(express.static('./client/build'))
+app.use(express.static('./client-ionic/www'))
 
 const server = app.listen(port, () => {
     console.log(`Server is up and listening on port ${port}.`)
