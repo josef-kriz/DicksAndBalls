7f133a0b527963f018d62a0d5ef73d3cde8ef241
Josef Kriz <email@josefkriz.com>
2020-11-29 03:23:02 +0100

    refactor room join & leave

--- src/listeners/gameListener.ts
+++ src/listeners/gameListener.ts
@@ -67,8 +67,10 @@ export function gameListener(socket: Socket): void {
 
     const joinTable = (message: JoinTableMessage, callback?: Function): void => {
         try {
-            socket.leave(tableId)
-            if (tableId !== message.id) tableId = message.id
+            if (tableId !== message.id) {
+                socket.leave(tableId)
+                tableId = message.id
+            }
             socket.join(tableId)
             socket.emit('server_event', tables.getGame(tableId).getGameStateMessage())
         } catch (e) {
@@ -89,10 +91,7 @@ export function gameListener(socket: Socket): void {
             })
 
             callback && callback('success')
-            // Players were sometimes not present in a room despite correctly being added previously in joinTable()
-            // This led to the game state message not being sent and breaking the game on client side
-            // The cause of this problem couldn't be found but we just join the room again to be safe
-            socket.join(tableId, () => io.to(tableId).emit('server_event', tables.getGame(tableId).getGameStateMessage()))
+            io.to(tableId).emit('server_event', tables.getGame(tableId).getGameStateMessage())
             io.emit('table_event', tables.getTableUpdateMessage())
         } catch (e) {
             callback && callback(e.message)
